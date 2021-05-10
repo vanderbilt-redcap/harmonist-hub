a0590dfb6981acec798619fdbf87fd92692d00f8
knil-maloon <linkedfrends@gmail.com>
2021-05-10 14:08:49 -0500

    removed htmlentities

--- hub/hub_login.php
+++ hub/hub_login.php
@@ -4,7 +4,7 @@
 
     <div class="col-md-10">
         <div class="alert alert-info" id="blue-alert" style="display:none">
-            <?=htmlentities($settings['hub_login_blue_text'])?>
+            <?=$settings['hub_login_blue_text']?>
         </div>
         <div class="alert alert-danger" id="hub_error_message" style="display: none;"></div>
         <p></p>
