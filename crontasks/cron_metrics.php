94291cf6b1e5c5c07094e6be70bd8bad5c7c560b
knil-maloon <linkedfrends@gmail.com>
2021-07-28 10:36:18 -0500

    testing crons

--- crontasks/cron_metrics.php
+++ crontasks/cron_metrics.php
@@ -243,7 +243,9 @@ if($pidsArray['METRICS'] != "") {
     $arrayMetrics[0]['admins'] = $number_requests_admin;
     \REDCap::email('eva.bascompte.moragas@vumc.org', 'harmonist@vumc.org', "Metrics Cron IN", "Date: ".$arrayMetrics[0]['date']);
     $json = json_encode($arrayMetrics);
-    \REDCap::email('eva.bascompte.moragas@vumc.org', 'harmonist@vumc.org', "Metrics Cron IN",$arrayMetrics);
+
+    $message = "<ul><pre>".print_r($arrayMetrics,true)."</pre></ul>";
+    \REDCap::email('eva.bascompte.moragas@vumc.org', 'harmonist@vumc.org', "Metrics Cron IN",$message);
 //    $results = \Records::saveData($pidsArray['METRICS'], 'json', $json, 'overwrite', 'YMD', 'flat', '', true, true, true, false, true, array(), true, false);
 //    \Records::addRecordToRecordListCache($pidsArray['METRICS'], $record_id_metrics, 1);
 //    \REDCap::email('eva.bascompte.moragas@vumc.org', 'harmonist@vumc.org', "Metrics Cron JSON RESULTS", $results);
