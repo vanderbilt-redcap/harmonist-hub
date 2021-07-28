8c9e0363c3b53ff461c3800bb2cc5af6a2ae8893
knil-maloon <linkedfrends@gmail.com>
2021-07-28 10:43:32 -0500

    Cron Metrics fixed

--- HarmonistHubExternalModule.php
+++ HarmonistHubExternalModule.php
@@ -157,10 +157,6 @@ class HarmonistHubExternalModule extends AbstractExternalModule
 
                 if(!empty($pidsArray) && !empty($settings)) {
                     try {
-                        if ($cronAttributes['cron_name'] == 'cron_data_upload_notification'){
-                            include("crontasks/cron_metrics.php");
-                        }
-
                         #CRONS
                         if ($cronAttributes['cron_name'] == 'cron_metrics') {
                              \REDCap::email('eva.bascompte.moragas@vumc.org', 'harmonist@vumc.org', "CRON ".$cronAttributes['cron_name'], "PID: ".$project_id);
