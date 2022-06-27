0565746bb2302f60f65beb4008aefa67b0efec2b
knil-maloon <linkedfrends@gmail.com>
2022-06-27 13:38:20 -0500

    tests on email

--- email.php
+++ email.php
@@ -14,19 +14,19 @@ function sendEmail($to, $from, $fromName, $subject, $message, $record_id, $actio
         $environment = " ".ENVIRONMENT;
     }
 
-    $send = \REDCap::email ($to,  $from, $subject,  $message,  $cc,  '' ,  $fromName.$environment);
-    \REDCap::email ("eva.bascompte.moragas@vumc.org",  "harmonist@vumc.org", "test");
+//    $send = \REDCap::email ($to,  $from, $subject,  $message,  $cc,  '' ,  $fromName.$environment);
+    \REDCap::email ("eva.bascompte.moragas@vumc.org",  "harmonist@vumc.org", "test","my little test");
 
-    if (!$send) {
-        //datacore@vumc.org;
-        \REDCap::email('eva.bascompte.moragas@vumc.org;harmonist@vumc.org', 'harmonist@vumc.org',"Mailer Error:".
-            $action_description, "Mailer Error (send = ".$send."): the email could not be sent in project ".$pid." record #".$record_id.
-            "<br><br>To: ".$to."<br>CC: ".$cc."<br>From (".$fromName.$environment."): ".$from."<br>Subject: ".$subject.
-            "<br>Message: <br>".$message);
-    } else {
-        //Add some logs
-        $changes_made = "[record_id]:".$record_id.", [email]: ".$to;
-        \REDCap::logEvent($action_description,$changes_made,NULL,null,null,$pid);
-    }
+//    if (!$send) {
+//        //datacore@vumc.org;
+//        \REDCap::email('eva.bascompte.moragas@vumc.org;harmonist@vumc.org', 'harmonist@vumc.org',"Mailer Error:".
+//            $action_description, "Mailer Error (send = ".$send."): the email could not be sent in project ".$pid." record #".$record_id.
+//            "<br><br>To: ".$to."<br>CC: ".$cc."<br>From (".$fromName.$environment."): ".$from."<br>Subject: ".$subject.
+//            "<br>Message: <br>".$message);
+//    } else {
+//        //Add some logs
+//        $changes_made = "[record_id]:".$record_id.", [email]: ".$to;
+//        \REDCap::logEvent($action_description,$changes_made,NULL,null,null,$pid);
+//    }
 }
 ?>
