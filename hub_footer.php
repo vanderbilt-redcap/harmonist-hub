a03f0995f1cbab93afbfd31c0aad46202ada8b9b
knil-maloon <linkedfrends@gmail.com>
2022-10-17 09:02:53 -0500

    Added NOAUTH to report a bug

--- hub_footer.php
+++ hub_footer.php
@@ -10,5 +10,5 @@ $versionsByPrefix = $module->getEnabledModules($_GET['pid']);
 
 <div class="footer">
     <div>Powered by <a href="<?=$module->getUrl('index.php?NOAUTH&pid='.$pidsArray['PROJECTS'].'&option=log')?>">Harmonist Hub <?=$versionsByPrefix['harmonist-hub']?></a></div>
-    <div><a href="<?=$module->getUrl('index.php?NOAUTH&pid='.$pidsArray['PROJECTS'].'&option=abt')?>">About <?=$about['about_title']?></a> | <a href="mailto:<?=$settings['hub_contact_email']?>">Contact us</a> | <a href="<?=$module->getUrl('index.php?pid='.$pidsArray['PROJECTS'].'&option=bug')?>">Report a bug</a></div>
+    <div><a href="<?=$module->getUrl('index.php?NOAUTH&pid='.$pidsArray['PROJECTS'].'&option=abt')?>">About <?=$about['about_title']?></a> | <a href="mailto:<?=$settings['hub_contact_email']?>">Contact us</a> | <a href="<?=$module->getUrl('index.php?NOAUTH&pid='.$pidsArray['PROJECTS'].'&option=bug')?>">Report a bug</a></div>
 </div>
