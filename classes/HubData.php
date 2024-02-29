f801e06f14428e80290e7be6e61aa22b7a377567
bascome <eva.bascompte.moragas@vumc.org>
2024-02-29 08:56:53 -0600

    bug fix on ['last_logged_event'] SESSION

--- classes/HubData.php
+++ classes/HubData.php
@@ -48,11 +48,11 @@ class HubData
     {
         $project_id = $this->pidsArray['REGIONS'];
         $last_logged_event = \Project::getLastLoggedEvent($project_id, true);
-        if (empty($_SESSION[$this->session_name]['regions']) || ($_SESSION[$this->session_name]['regions']['last_logged_event'] != $last_logged_event)) {
+        if (empty($_SESSION[$this->session_name]['regions']) || ($_SESSION[$this->session_name]['last_logged_event']['regions'] != $last_logged_event)) {
             $regions = \REDCap::getData($project_id, 'json-array', null,null,null,null,false,false,false,"[showregion_y] =1");
             ArrayFunctions::array_sort_by_column($regions, 'region_code');
             $_SESSION[$this->session_name]['regions'] = $regions;
-            $_SESSION[$this->session_name]['regions']['last_logged_event'] = $last_logged_event;
+            $_SESSION[$this->session_name]['last_logged_event']['regions'] = $last_logged_event;
         }
         return $_SESSION[$this->session_name]['regions'];
     }
@@ -62,12 +62,12 @@ class HubData
 		
 		## Check if session data is already set and if a logged event (record update/creation) has happened since the cache was set
 		$last_logged_event = \Project::getLastLoggedEvent($project_id, true);
-		if (empty($_SESSION[$this->session_name]['requests']) || ($_SESSION[$this->session_name]['requests']['last_logged_event'] != $last_logged_event)) {
+		if (empty($_SESSION[$this->session_name]['requests']) || ($_SESSION[$this->session_name]['last_logged_event']['requests'] != $last_logged_event)) {
 			$RecordSetRM = \REDCap::getData($project_id, 'array', null);
 			$requests = ProjectData::getProjectInfoArrayRepeatingInstruments($RecordSetRM, array('approval_y' => 1));
 			ArrayFunctions::array_sort_by_column($requests, 'due_d');
 			$_SESSION[$this->session_name]['requests'] = $requests;
-			$_SESSION[$this->session_name]['requests']['last_logged_event'] = $last_logged_event;
+			$_SESSION[$this->session_name]['last_logged_event']['requests'] = $last_logged_event;
 		}
 		
         return $_SESSION[$this->session_name]['requests'];
