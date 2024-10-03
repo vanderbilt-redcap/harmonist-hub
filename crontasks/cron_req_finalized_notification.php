25750c44bd9fbf29c8759ad159a332a9626b7194
bascome <eva.bascompte.moragas@vumc.org>
2024-10-03 15:33:56 -0500

    fixed crons as pid was not being found

--- crontasks/cron_req_finalized_notification.php
+++ crontasks/cron_req_finalized_notification.php
@@ -2,18 +2,20 @@
 namespace Vanderbilt\HarmonistHubExternalModule;
 require_once(dirname(dirname(__FILE__))."/classes/AllCrons.php");
 
-$settings = \REDCap::getData($pidsArray['SETTINGS'], 'json-array', null)[0];
-
-$requests = \REDCap::getData($pidsArray['RMANAGER'], 'json-array',null, null, null, null, false, false, false,
-    "[finalize_y] = 1 AND [finalize_noemail] != 1 AND [request_summary_sent_y(1)] != 1");
-foreach ($requests as $request) {
-    $message = AllCrons::runCronReqFinalizedNotification(
-        $this,
-        $pidsArray,
-        $request,
-        $settings,
-        true
-    );
+if(is_numeric($pidsArray['RMANAGER']) && is_numeric($pidsArray['SETTINGS'])) {
+    $settings = \REDCap::getData($pidsArray['SETTINGS'], 'json-array', null)[0];
+    $requests = \REDCap::getData($pidsArray['RMANAGER'], 'json-array', null, null, null, null, false, false, false, "[finalize_y] = 1 AND [finalize_noemail] != 1 AND [request_summary_sent_y(1)] != 1");
+    foreach ($requests as $request) {
+        $message = AllCrons::runCronReqFinalizedNotification(
+            $this,
+            $pidsArray,
+            $request,
+            $settings,
+            true
+        );
+    }
+}else{
+    error_log("IeDEA CRON CronReqFinalizedNotification Failed PID: ".$project_id.", PROJECTS: ".$pidsArray['PROJECTS'].", RMANAGER: ".$pidsArray['RMANAGER'].", SETTINGS: ".$pidsArray['SETTINGS']);
+    return;  // Stop execution and avoid emailing anyone else errors, since we know the following getData() is going to fail anyway
 }
-
 ?>
