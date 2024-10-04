74a9b2246784dcad0e65900ff8f6f3937a54ba31
bascome <eva.bascompte.moragas@vumc.org>
2024-10-04 11:11:21 -0500

    indent fix

--- crontasks/cron_data_upload_notification.php
+++ crontasks/cron_data_upload_notification.php
@@ -8,18 +8,18 @@ $settings = \REDCap::getData($pidsArray['SETTINGS'], 'json-array', null)[0];
 $days_expiration = intval($settings['uploadnotification_dur']);
 $extra_days = ' + ' . $days_expiration . " days";
 foreach ($request_DU as $upload) {
-$RecordSetSOP = \REDCap::getData($pidsArray['SOP'], 'array', array('record_id' => $upload['data_assoc_request']));
-$sop = ProjectData::getProjectInfoArrayRepeatingInstruments($RecordSetSOP, $pidsArray['SOP'])[0];
+    $RecordSetSOP = \REDCap::getData($pidsArray['SOP'], 'array', array('record_id' => $upload['data_assoc_request']));
+    $sop = ProjectData::getProjectInfoArrayRepeatingInstruments($RecordSetSOP, $pidsArray['SOP'])[0];
 
-$message = AllCrons::runCronDataUploadNotification(
-    $this,
-    $pidsArray,
-    $upload,
-    $sop,
-    null,
-    $extra_days,
-    $settings,
-    true
-);
+    $message = AllCrons::runCronDataUploadNotification(
+        $this,
+        $pidsArray,
+        $upload,
+        $sop,
+        null,
+        $extra_days,
+        $settings,
+        true
+    );
 }
 ?>
