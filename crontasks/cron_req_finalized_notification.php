e5a660c254f6a0f074f46637055993500716fea1
bascome <eva.bascompte.moragas@vumc.org>
2024-10-07 08:03:02 -0500

    fixed removed projects class

--- crontasks/cron_req_finalized_notification.php
+++ crontasks/cron_req_finalized_notification.php
@@ -1,6 +1,7 @@
 <?php
 namespace Vanderbilt\HarmonistHubExternalModule;
 require_once(dirname(dirname(__FILE__))."/classes/AllCrons.php");
+include_once(__DIR__ ."/../projects.php");
 
 $settings = \REDCap::getData($pidsArray['SETTINGS'], 'json-array', null)[0];
 $requests = \REDCap::getData($pidsArray['RMANAGER'], 'json-array', null, null, null, null, false, false, false, "[finalize_y] = 1 AND [finalize_noemail] != 1 AND [request_summary_sent_y(1)] != 1");
