c17ced4039c9b8cd23a7670ca6b048325f8acada
bascome <eva.bascompte.moragas@vumc.org>
2024-10-07 14:38:56 -0500

    refactor record id name

--- sop/sop_step_1_save_AJAX.php
+++ sop/sop_step_1_save_AJAX.php
@@ -4,7 +4,7 @@ require_once dirname(dirname(__FILE__))."/projects.php";
 
 $selectConcept = htmlentities($_REQUEST['selectConcept'],ENT_QUOTES);
 $option = htmlentities($_REQUEST['option'],ENT_QUOTES);
-$record = htmlentities($_REQUEST['id'],ENT_QUOTES);
+$record_id = htmlentities($_REQUEST['id'],ENT_QUOTES);
 $save_option = htmlentities($_REQUEST['save_option'],ENT_QUOTES);
 $sop_hubuser = htmlentities($_REQUEST['sop_hubuser'],ENT_QUOTES);
 
@@ -21,20 +21,20 @@ if($option == "1" && $save_option == ""){
     #NEW SOP
     $Proj = new \Project($pidsArray['SOP']);
     $event_id = $Proj->firstEventId;
-    $record = $module->framework->addAutoNumberedRecord($pidsArray['SOP']);
-    $save_option = $record;
+    $record_id = $module->framework->addAutoNumberedRecord($pidsArray['SOP']);
+    $save_option = $record_id;
 
     $arraySOP = array();
-    $arraySOP[$record][$event_id]['sop_status'] = "0";//DRAFT
-    $arraySOP[$record][$event_id]['sop_hubuser'] = $sop_hubuser;
-    $arraySOP[$record][$event_id]['sop_concept_id'] = $selectConcept;
-    $arraySOP[$record][$event_id]['sop_visibility'] = "1";
-    $arraySOP[$record][$event_id]['sop_active'] = "1";
+    $arraySOP[$record_id][$event_id]['sop_status'] = "0";//DRAFT
+    $arraySOP[$record_id][$event_id]['sop_hubuser'] = $sop_hubuser;
+    $arraySOP[$record_id][$event_id]['sop_concept_id'] = $selectConcept;
+    $arraySOP[$record_id][$event_id]['sop_visibility'] = "1";
+    $arraySOP[$record_id][$event_id]['sop_active'] = "1";
 
-    $sop_name = $record.". Data Request for ".$concept_id.", ".$concept_title;
-    $arraySOP[$record][$event_id]['sop_name'] = $sop_name;
-    $arraySOP[$record][$event_id]['sop_created_dt'] = $sop_created_dt;
-    $arraySOP[$record][$event_id]['sop_updated_dt'] = $sop_created_dt;
+    $sop_name = $record_id.". Data Request for ".$concept_id.", ".$concept_title;
+    $arraySOP[$record_id][$event_id]['sop_name'] = $sop_name;
+    $arraySOP[$record_id][$event_id]['sop_created_dt'] = $sop_created_dt;
+    $arraySOP[$record_id][$event_id]['sop_updated_dt'] = $sop_created_dt;
 
     $RecordSetSOP = \REDCap::getData($pidsArray['SOP'], 'array', null,null,null,null,false,false,false,"[sop_active] = 1 AND [sop_status] = 0 AND [sop_hubuser] = '".$sop_hubuser."'");
     $sop_drafts = ProjectData::getProjectInfoArrayRepeatingInstruments($RecordSetSOP,$pidsArray['SOP']);
@@ -46,7 +46,7 @@ if($option == "1" && $save_option == ""){
             if($draft['sop_active'] == '1') {
                 $RecordSetConcepts = \REDCap::getData($pidsArray['HARMONIST'], 'array', array("record_id" => $draft['sop_concept_id']));
                 $concept_id = ProjectData::getProjectInfoArrayRepeatingInstruments($RecordSetConcepts,$pidsArray['HARMONIST'])[0]['concept_id'];
-                if($draft['record_id'] == $record){
+                if($draft['record_id'] == $record_id){
                     $data_select .=  "<option value='" . $draft['record_id'] . "' concept='" . $draft['sop_concept_id'] . "' concept_id='" . $concept_id . "' selected>" . $draft['sop_name'] . "</option>";
                 }else{
                     $data_select .=  "<option value='" . $draft['record_id'] . "' concept='" . $draft['sop_concept_id'] . "' concept_id='" . $concept_id . "'>" . $draft['sop_name'] . "</option>";
@@ -58,45 +58,45 @@ if($option == "1" && $save_option == ""){
     }
 }else if($option == "2"){
     #LOAD TEMPLATE
-    $RecordSetSOP = \REDCap::getData($pidsArray['SOP'], 'array', array("record_id" => $record));
+    $RecordSetSOP = \REDCap::getData($pidsArray['SOP'], 'array', array("record_id" => $record_id));
     $sop = ProjectData::getProjectInfoArrayRepeatingInstruments($RecordSetSOP,$pidsArray['SOP'])[0];
 
     $Proj = new \Project($pidsArray['SOP']);
     $event_id = $Proj->firstEventId;
-    $record = $module->framework->addAutoNumberedRecord($pidsArray['SOP']);
-    $save_option = $record;
+    $record_id = $module->framework->addAutoNumberedRecord($pidsArray['SOP']);
+    $save_option = $record_id;
 
     $arraySOP = array();
-    $arraySOP[$record][$event_id]['sop_status'] = "0";//DRAFT
-    $arraySOP[$record][$event_id]['sop_hubuser'] = $sop['sop_hubuser'];
-    $arraySOP[$record][$event_id]['sop_concept_id'] = $sop['sop_concept_id'];
-    $arraySOP[$record][$event_id]['sop_tablefields'] = $sop['sop_tablefields'];
-    $arraySOP[$record][$event_id]['sop_downloaders'] = $sop['downloaders'];
-    $arraySOP[$record][$event_id]['sop_inclusion'] = $sop['sop_inclusion'];
-    $arraySOP[$record][$event_id]['sop_exclusion'] = $sop['sop_exclusion'];
-    $arraySOP[$record][$event_id]['sop_notes'] = $sop['sop_notes'];
-    $arraySOP[$record][$event_id]['sop_creator'] = $sop['sop_creator'];
-    $arraySOP[$record][$event_id]['sop_creator2'] = $sop['sop_creator2'];
-    $arraySOP[$record][$event_id]['sop_datacontact'] = $sop['sop_datacontact'];
-    $arraySOP[$record][$event_id]['sop_extrapdf'] = $sop['sop_extrapdf'];
-    $arraySOP[$record][$event_id]['sop_finalpdf'] = $sop['sop_finalpdf'];
-    $arraySOP[$record][$event_id]['sop_visibility'] = "1";
-    $arraySOP[$record][$event_id]['sop_active'] = "1";
-    $arraySOP[$record][$event_id]['dataformat_prefer'] = $sop['dataformat_prefer'];
-    $arraySOP[$record][$event_id]['dataformat_notes'] = $sop['dataformat_notes'];
+    $arraySOP[$record_id][$event_id]['sop_status'] = "0";//DRAFT
+    $arraySOP[$record_id][$event_id]['sop_hubuser'] = $sop['sop_hubuser'];
+    $arraySOP[$record_id][$event_id]['sop_concept_id'] = $sop['sop_concept_id'];
+    $arraySOP[$record_id][$event_id]['sop_tablefields'] = $sop['sop_tablefields'];
+    $arraySOP[$record_id][$event_id]['sop_downloaders'] = $sop['downloaders'];
+    $arraySOP[$record_id][$event_id]['sop_inclusion'] = $sop['sop_inclusion'];
+    $arraySOP[$record_id][$event_id]['sop_exclusion'] = $sop['sop_exclusion'];
+    $arraySOP[$record_id][$event_id]['sop_notes'] = $sop['sop_notes'];
+    $arraySOP[$record_id][$event_id]['sop_creator'] = $sop['sop_creator'];
+    $arraySOP[$record_id][$event_id]['sop_creator2'] = $sop['sop_creator2'];
+    $arraySOP[$record_id][$event_id]['sop_datacontact'] = $sop['sop_datacontact'];
+    $arraySOP[$record_id][$event_id]['sop_extrapdf'] = $sop['sop_extrapdf'];
+    $arraySOP[$record_id][$event_id]['sop_finalpdf'] = $sop['sop_finalpdf'];
+    $arraySOP[$record_id][$event_id]['sop_visibility'] = "1";
+    $arraySOP[$record_id][$event_id]['sop_active'] = "1";
+    $arraySOP[$record_id][$event_id]['dataformat_prefer'] = $sop['dataformat_prefer'];
+    $arraySOP[$record_id][$event_id]['dataformat_notes'] = $sop['dataformat_notes'];
 
-    $sop_name = $record.". Data Request for ".$concept_id.", ".$concept_title;
-    $arraySOP[$record][$event_id]['sop_name'] = $sop_name;
-    $arraySOP[$record][$event_id]['sop_created_dt'] = $sop_created_dt;
-    $arraySOP[$record][$event_id]['sop_updated_dt'] = $sop_created_dt;
+    $sop_name = $record_id.". Data Request for ".$concept_id.", ".$concept_title;
+    $arraySOP[$record_id][$event_id]['sop_name'] = $sop_name;
+    $arraySOP[$record_id][$event_id]['sop_created_dt'] = $sop_created_dt;
+    $arraySOP[$record_id][$event_id]['sop_updated_dt'] = $sop_created_dt;
 }else{
     #LOAD DRAFT
     if($option == "1"){
-        $record = $save_option;
+        $record_id = $save_option;
     }else{
-        $save_option = $record;
+        $save_option = $record_id;
     }
-    $RecordSetSOP = \REDCap::getData($pidsArray['SOP'], 'array', array("record_id" => $record));
+    $RecordSetSOP = \REDCap::getData($pidsArray['SOP'], 'array', array("record_id" => $record_id));
     $sop = ProjectData::getProjectInfoArrayRepeatingInstruments($RecordSetSOP,$pidsArray['SOP'])[0];
 
     $RecordSetConcepts = \REDCap::getData($pidsArray['HARMONIST'], 'array', array("record_id" => $sop['sop_concept_id']));
@@ -107,17 +107,17 @@ if($option == "1" && $save_option == ""){
         $event_id = $Proj->firstEventId;
 
         $arraySOP = array();
-        $arraySOP[$record][$event_id]['sop_concept_id'] = $selectConcept;
-        $sop_name = $record.". Data Request for ".$concept_id.", ".$concept_title;
-        $arraySOP[$record][$event_id]['sop_name'] = $sop_name;
-        $arraySOP[$record][$event_id]['sop_updated_dt'] = $sop_created_dt;
-        $arraySOP[$record][$event_id]['sop_due_d'] = $sop['sop_due_d'];
+        $arraySOP[$record_id][$event_id]['sop_concept_id'] = $selectConcept;
+        $sop_name = $record_id.". Data Request for ".$concept_id.", ".$concept_title;
+        $arraySOP[$record_id][$event_id]['sop_name'] = $sop_name;
+        $arraySOP[$record_id][$event_id]['sop_updated_dt'] = $sop_created_dt;
+        $arraySOP[$record_id][$event_id]['sop_due_d'] = $sop['sop_due_d'];
     }
 }
 $results = \Records::saveData($pidsArray['SOP'], 'array', $arraySOP,'overwrite', 'YMD', 'flat', '', true, true, true, false, true, array(), true, false);
-\Records::addRecordToRecordListCache($pidsArray['SOP'], $record,1);
+\Records::addRecordToRecordListCache($pidsArray['SOP'], $record_id,1);
 
-$data = \REDCap::getData($pidsArray['SOP'], 'json-array', array("record_id" => $record))[0];
+$data = \REDCap::getData($pidsArray['SOP'], 'json-array', array("record_id" => $record_id))[0];
 
 $data['select'] = $data_select;
 $data['save_option'] = $save_option;
@@ -126,7 +126,7 @@ $date = new \DateTime($sop['sop_due_d']);
 $data['sop_due_d_preview'] = $date->format('d F Y');
 
 $data['concept_id'] = $concept_id;
-$data['record_id'] = $record;
+$data['record_id'] = $record_id;
 
 #Load information for STEP4
 $dataformat_prefer_text = "";
