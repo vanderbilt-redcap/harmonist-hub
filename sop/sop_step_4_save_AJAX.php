3e32083e3f3b6dd710335c8207755cb32fd56dd2
bascome <eva.bascompte.moragas@vumc.org>
2024-10-08 09:40:52 -0500

    Fixed code lists for pdf and removed unnecessary path

--- sop/sop_step_4_save_AJAX.php
+++ sop/sop_step_4_save_AJAX.php
@@ -25,14 +25,14 @@ foreach ($regions as $region){
     }
 }
 
-$dataTable = \Vanderbilt\HarmonistHubExternalModule\getTablesInfo($module, $pidsArray['DATAMODEL']);
+$dataTable = getTablesInfo($module, $pidsArray['DATAMODEL']);
 $tableHtml = "";
 if(!empty($dataTable)) {
     # Get selected rows
-    $tableHtml = \Vanderbilt\HarmonistHubExternalModule\generateTablesHTML_pdf($module, $pidsArray['CODELIST'], $dataTable,$sop['sop_tablefields']);
-    $requested_tables = \Vanderbilt\HarmonistHubExternalModule\generateRequestedTablesList_pdf($dataTable,$sop['sop_tablefields']);
+    $tableHtml = generateTablesHTML_pdf($module, $pidsArray['CODELIST'], $dataTable,$sop['sop_tablefields']);
+    $requested_tables = generateRequestedTablesList_pdf($dataTable,$sop['sop_tablefields']);
 
-    $dataTable = \Vanderbilt\HarmonistHubExternalModule\getTablesInfo($module, $pidsArray['DATAMODEL']);
+    $dataTable = getTablesInfo($module, $pidsArray['DATAMODEL']);
     $tablefields = array();
     foreach( $dataTable as $data ) {
         if (!empty($data['record_id'])) {
@@ -153,7 +153,7 @@ $second_page .= "<p><span style='font-size: 12pt'>".$requested_tables."</span></
 
 $page_num = '<style>.footer .page-number:after { content: counter(page); } .footer { position: fixed; bottom: 0px;color:grey }a{text-decoration: none;}</style>';
 
-$img = \Vanderbilt\HarmonistHubExternalModule\getFile($module, $pidsArray['PROJECTS'], $settings['hub_logo_pdf'],'src');
+$img = getFile($module, $pidsArray['PROJECTS'], $settings['hub_logo_pdf'],'src');
 
 $html_pdf = "<html><body style='font-family:\"Calibri\";font-size:10pt;'>".$page_num
     ."<div class='footer'><span left: 0px;>".$concept_id."</span></div>"
