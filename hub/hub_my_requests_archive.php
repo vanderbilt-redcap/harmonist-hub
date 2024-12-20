81e8193c509e7fa527ea3ef824829ab335f7aeec
bascome <eva.bascompte.moragas@vumc.org>
2024-12-20 09:39:03 -0600

    fix on type. There was issue when hidden choices did not exist

--- hub/hub_my_requests_archive.php
+++ hub/hub_my_requests_archive.php
@@ -94,9 +94,9 @@ if($_REQUEST['type'] != ""){
                 <select class="form-control" name="selectReqType" id="selectReqType">
                     <option value="">Select All</option>
                     <?php
-                    if(!empty($request_type_label) && is_array($default_values->getHideChoice($pidsArray['RMANAGER'])[$pidsArray['RMANAGER']]['request_type'])){
+                    if(!empty($request_type_label) && is_array($default_values->getHideChoice($pidsArray['RMANAGER'])[$pidsArray['RMANAGER']]['request_type'])  || empty($default_values->getHideChoice($pidsArray['RMANAGER'])[$pidsArray['RMANAGER']]['request_type'])){
                         foreach ($request_type_label as $value => $label) {
-                            if (!in_array($value, $default_values->getHideChoice($pidsArray['RMANAGER'])[$pidsArray['RMANAGER']]['request_type'])) {
+                            if ((is_array($default_values->getHideChoice($pidsArray['RMANAGER'])[$pidsArray['RMANAGER']]['request_type']) && !in_array($value, $default_values->getHideChoice($pidsArray['RMANAGER'])[$pidsArray['RMANAGER']]['request_type'])) || empty($default_values->getHideChoice($pidsArray['RMANAGER'])[$pidsArray['RMANAGER']]['request_type'])) {
                                 echo "<option value='" . $label . "'>" . $label . "</option>";
                             }
                         }
