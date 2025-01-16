9f31f5287fd076672efdaa8c25ca141499e54bd4
bascome <eva.bascompte.moragas@vumc.org>
2025-01-16 08:35:29 -0600

    removed Security Handler Request on index

--- index.php
+++ index.php
@@ -15,7 +15,6 @@ $hub_profile = $module->getProjectSetting('hub-profile');
 
 $pid = $module->getSecurityHandler()->getProjectId();
 $option = $module->getSecurityHandler()->getRequestOption();
-$requestUrl = $module->getSecurityHandler()->getRequestUrl();
 $is_authorized_and_has_rights = false;
 if ($module->getSecurityHandler()->isAuthorizedPage()) {
     $pidsArray = $module->getSecurityHandler()->getPidsArray();
@@ -36,7 +35,7 @@ if ($module->getSecurityHandler()->isAuthorizedPage()) {
 <html lang="en">
 <?php
 if($hub_projectname != '' && $hub_profile != ''){
-        if(array_key_exists('message',$requestUrl) && $requestUrl['message']=='DD'){?>
+        if(array_key_exists('message',$_REQUEST) && $_REQUEST['message']=='DD'){?>
             <div class="container" style="margin-top: 80px">
                 <div class="alert alert-success col-md-12">
                     Data Dictionary and projects successfully installed. To see the Project Ids go to the <a href="<?=APP_PATH_WEBROOT?>DataEntry/record_status_dashboard.php?pid=<?=$pid?>" target="_blank">Record Dashboard</a>.
@@ -79,7 +78,7 @@ if($hub_projectname != '' && $hub_profile != ''){
 
                 $token = $module->getSecurityHandler()->getTokenSession();
 
-                if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'dfq'){
+                if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'dfq'){
                     //No header
                 }else{
                     include('hub_header.php');
@@ -87,70 +86,70 @@ if($hub_projectname != '' && $hub_profile != ''){
                 ?>
                 <div class="container" style="margin: 0 auto;float:none;min-height: 900px;">
                     <?php
-                    if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'map' )
+                    if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'map' )
                     {
                         include('map/index.php');
-                    }else if( !array_key_exists(SecurityHandler::SESSION_TOKEN_STRING, $requestUrl) && !array_key_exists('request', $requestUrl) && empty($_SESSION[SecurityHandler::SESSION_TOKEN_STRING][$module->getSecurityHandler()->getTokenSessionName()])){
+                    }else if( !array_key_exists(SecurityHandler::SESSION_TOKEN_STRING, $_REQUEST) && !array_key_exists('request', $_REQUEST) && empty($_SESSION[SecurityHandler::SESSION_TOKEN_STRING][$module->getSecurityHandler()->getTokenSessionName()])){
                         include('hub/hub_login.php');
                     }else if($current_user['active_y'] == "0"){
                         include('hub/hub_login.php');
                     }else if(!empty($_SESSION[SecurityHandler::SESSION_TOKEN_STRING][$module->getSecurityHandler()->getTokenSessionName()]) && isTokenCorrect($_SESSION[SecurityHandler::SESSION_TOKEN_STRING][$module->getSecurityHandler()->getTokenSessionName()],$pidsArray['PEOPLE'])){
-                        if( !array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl)){
+                        if( !array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST)){
                             include('hub/hub_home.php');
                         }
-                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'log')
+                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'log')
                         {
                             include('hub/hub_changelog.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'smn' && !$deactivate_datahub)
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'smn' && !$deactivate_datahub)
                         {
                             include('sop/sop_request_data.php');
-                        } else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'sra' && !$deactivate_datahub)
+                        } else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'sra' && !$deactivate_datahub)
                         {
                             include('sop/sop_recent_activity.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'tbl' && !$deactivate_datahub && !$deactivate_tblcenter)
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'tbl' && !$deactivate_datahub && !$deactivate_tblcenter)
                         {
                             include('sop/sop_table_center.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'ofs' && !$deactivate_datahub)
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'ofs' && !$deactivate_datahub)
                         {
                             include('sop/sop_document_library.php');
                         }
-                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'sop' && !$deactivate_datahub)
+                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'sop' && !$deactivate_datahub)
                         {
                             include('sop/sop_data_request_title.php');
                         }
-                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'dna' && !$deactivate_datahub)
+                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'dna' && !$deactivate_datahub)
                         {
                             include('sop/sop_news_archive.php');
                         }
-                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'ss1' && !$deactivate_datahub)
+                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'ss1' && !$deactivate_datahub)
                         {
                             include('sop/sop_steps_menu.php');
                         }
-                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'ss5' && !$deactivate_datahub)
+                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'ss5' && !$deactivate_datahub)
                         {
                             include('sop/sop_step_5.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'spr' && !$deactivate_datahub)
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'spr' && !$deactivate_datahub)
                         {
                             include('sop/sop_make_public_request_review.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'lgd' && !$deactivate_datahub)
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'lgd' && !$deactivate_datahub)
                         {
                             include('sop/sop_data_activity_log.php');
-                        } else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'cpt' )
+                        } else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'cpt' )
                         {
                             include('hub/hub_concepts.php');
                         }
-                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'ttl' )
+                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'ttl' )
                         {
                             include('hub/hub_concept_title.php');
                         }
-                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'hub')
+                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'hub')
                         {
-                            if(array_key_exists('record', $requestUrl) && !empty($requestUrl['record'])) {
+                            if(array_key_exists('record', $_REQUEST) && !empty($_REQUEST['record'])) {
                                 include('hub/hub_request_title.php');
                             }else{
                                 include('hub/hub_requests.php');
                             }
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'adm')
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'adm')
                         {
                             if($isAdmin){
                                 include('hub/hub_admin.php');
@@ -158,59 +157,59 @@ if($hub_projectname != '' && $hub_profile != ''){
                                 include('hub/hub_error_page.php');
                             }
                         }
-                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'usr'){
+                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'usr'){
                             include('hub/hub_users.php');
                         }
-                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'mra')
+                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'mra')
                         {
                             include('hub/hub_my_requests_archive.php');
-                        } else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'mrr')
+                        } else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'mrr')
                         {
                             include('hub/hub_my_requests_archive_rejected.php');
                         }
-                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'hra')
+                        else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'hra')
                         {
                             include('hub/hub_recent_activity.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'upd' && !$deactivate_datahub)
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'upd' && !$deactivate_datahub)
                         {
                             include('sop/sop_submit_data.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'dat' && !$deactivate_datahub)
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'dat' && !$deactivate_datahub)
                         {
                             include('hub/hub_data.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'pdc' && !$deactivate_datahub)
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'pdc' && !$deactivate_datahub)
                         {
                             include('sop/sop_data_call_archive.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'out')
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'out')
                         {
                             include('hub/hub_publications.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'mts' && ($settings['deactivate_metrics___1'] != "1" || $isAdmin))
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'mts' && ($settings['deactivate_metrics___1'] != "1" || $isAdmin))
                         {
                             include('hub/hub_metrics_stats.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'mth' && ($settings['deactivate_datametrics___1'] != "1" || $isAdmin))
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'mth' && ($settings['deactivate_datametrics___1'] != "1" || $isAdmin))
                         {
                             include('sop/sop_metrics_stats.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'faq')
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'faq')
                         {
                             include('faq/hub_faq.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'pro')
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'pro')
                         {
                             include('hub/hub_profile.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'bug')
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'bug')
                         {
                             include('hub/hub_report_bug.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'unf')
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'unf')
                         {
                             include('hub/hub_request_title.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'und' && !$deactivate_datahub)
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'und' && !$deactivate_datahub)
                         {
                             include('sop/sop_data_request_title.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'cal' && $settings['calendar_active'][1] == "1")
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'cal' && $settings['calendar_active'][1] == "1")
                         {
                             include('hub/hub_calendar.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $option === 'abt')
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $option === 'abt')
                         {
                             include('hub/hub_about.php');
-                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $requestUrl) && $requestUrl[SecurityHandler::SESSION_OPTION_STRING] === 'dab' && !$deactivate_toolkit)
+                        }else if( array_key_exists(SecurityHandler::SESSION_OPTION_STRING, $_REQUEST) && $_REQUEST[SecurityHandler::SESSION_OPTION_STRING] === 'dab' && !$deactivate_toolkit)
                         {
                             include('sop/sop_explore_data.php');
                         }
