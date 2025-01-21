9516d94e2da502c90d311929e8d0b65bf6581b9e
bascome <eva.bascompte.moragas@vumc.org>
2025-01-21 09:21:21 -0600

    testing issue with email downloads

--- hooks/save_record_commentsAndVotes.php
+++ hooks/save_record_commentsAndVotes.php
@@ -136,7 +136,14 @@ if(($comment[$instrument.'_complete'] == '2' || $vanderbilt_emailTrigger->getEma
                 if(!empty($comment['extra_revfile2'])){
                     $gd_files .= "<li>".getFileLink($this, $pidsArray['PROJECTS'], $comment['extra_revfile2'],'',0,$secret_key,$secret_iv,$people['record_id'],"")."</li>";
                 }
+                if(empty($secret_key)){
+                    $gd_files .= "key empty";
+                }
+                if(empty($secret_iv)){
+                    $gd_files .= "iv empty";
+                }
                 $gd_files .= "</ol>";
+
 
                 /*** GROUP DISCUSION ***/
                 $text = "";
