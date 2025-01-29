545204560f9cc042bb28f8c2766021cf5cfd25f6
bascome <eva.bascompte.moragas@vumc.org>
2025-01-29 13:29:17 -0600

    adding prints

--- classes/SecurityHandler.php
+++ classes/SecurityHandler.php
@@ -291,11 +291,14 @@ class SecurityHandler
 
     public function getCredentialsServerVars($type)
     {
+        print_array(self::CREDENTIALS_PATH . $this->getPidsArray()['PROJECTS'] . constant(
+                        "self::CREDENTIALS_" . strtoupper($type) . "_FILENAME"));
         if (file_exists(
             self::CREDENTIALS_PATH . $this->getPidsArray()['PROJECTS'] . constant(
                 "self::CREDENTIALS_" . strtoupper($type) . "_FILENAME"
             )
         )) {
+            print_array("IN");
             require_once self::CREDENTIALS_PATH . $this->getPidsArray()['PROJECTS'] . constant(
                     "self::CREDENTIALS_" . strtoupper($type) . "_FILENAME"
                 );
