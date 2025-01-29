8a793042102bb34e5514f5b9dfcadd7a3d34e738
bascome <eva.bascompte.moragas@vumc.org>
2025-01-29 13:15:05 -0600

    adding prints

--- hub/aws/AWS_downloadFile.php
+++ hub/aws/AWS_downloadFile.php
@@ -6,6 +6,7 @@ include_once(dirname(dirname(dirname(__FILE__))) . "/email.php");
 include_once(dirname(dirname(dirname(__FILE__))) . "/classes/SecurityHandler.php");
 
 if ($module->getSecurityHandler()->isAuthorizedPage()) {
+    $pidsArray = $module->getSecurityHandler()->getPidsArray();
     $settings = $module->getSecurityHandler()->getSettingsData();
     if($settings['deactivate_datadown___1'] != "1" && $settings['deactivate_datahub___1'] != "1") {
         $module->getSecurityHandler()->getCredentialsServerVars("ENCRYPTION");
