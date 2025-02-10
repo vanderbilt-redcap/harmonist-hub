bd000e7ee3684ea20b001583bce7a3b397012618
bascome <eva.bascompte.moragas@vumc.org>
2025-02-10 14:52:19 -0600

    Added research group instances

--- hooks/save_record_concept_sheet.php
+++ hooks/save_record_concept_sheet.php
@@ -14,12 +14,14 @@ if($instrument == 'concept_sheet'){
         $instance = $region['record_id'];
         //only if it's the first time we save the info
         if(empty($concept[$record]['repeat_instances']['writing_group_by_research_group'][$instance])) {
-            $array_repeat_instances = array();
-            $aux = array();
-            $aux['gmember_role'] = $region['record_id'];
-            $aux['writing_group_by_research_group_complete'] = '1';
+            $array_repeat_instances = [];
 
-            $array_repeat_instances[$record]['repeat_instances'][$event_id]['writing_group_by_research_group'][$instance] = $aux;
+            $params = [
+                'gmember_role' =>  $region['record_id'],
+                'writing_group_by_research_group_complete' => '1'
+            ];
+
+            $array_repeat_instances[$record]['repeat_instances'][$event_id]['writing_group_by_research_group'][$instance] = $params;
             $results = \REDCap::saveData($project_id, 'array', $array_repeat_instances,'overwrite', 'YMD', 'flat', '', true, true, true, false, true, array(), true, false, 1, false, '');
             \REDCap::logEvent("Create Writing Group Instance\nConcept Sheet", $region['region_name']." (".$region['region_code'].")", null, $record, $event_id, $project_id);
         }else{
