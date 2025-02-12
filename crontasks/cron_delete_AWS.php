0b53203bf0cd73e799b689dbf6a4977455ad753d
bascome <eva.bascompte.moragas@vumc.org>
2025-02-12 10:11:39 -0600

    fix on splunk errors

--- crontasks/cron_delete_AWS.php
+++ crontasks/cron_delete_AWS.php
@@ -1,14 +1,11 @@
 <?php
 namespace Vanderbilt\HarmonistHubExternalModule;
 require_once(dirname(dirname(__FILE__))."/classes/AllCrons.php");
-require_once(dirname(dirname(__FILE__))."/classes/SecurityHandler.php");
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
 
-$this->securityHandler = new SecurityHandler($this, $project_id);
-$aws_credentials = $this->getSecurityHandler()->getCredentialsServerVars("AWS");
-// If the URL exists, then we have the credentials in the server and can continue
-if($aws_credentials != null) {
+$aws_credentials = "/app001/credentials/Harmonist-Hub/".$pidsArray['PROJECTS']."_aws_s3.php";
+if (file_exists($aws_credentials)){
     require_once ($aws_credentials);
     $credentials = new Aws\Credentials\Credentials($aws_key, $aws_secret);
     $s3 = new S3Client([
