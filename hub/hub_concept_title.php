6170cae978d653baed8a7312bc213ad1767d5c3a
bascome <eva.bascompte.moragas@vumc.org>
2025-02-21 08:26:28 -0600

    fixed linked docs

--- hub/hub_concept_title.php
+++ hub/hub_concept_title.php
@@ -591,7 +591,7 @@ if ((!empty($concept) && $concept['adminupdate_d'] != "" && count($concept['admi
     <div class="panel panel-default" style="margin-bottom: 40px">
         <div class="panel-heading">
             <h3 class="panel-title">
-                <a data-toggle="collapse" href="#collapse_publications">Linked Documents</a>
+                <a data-toggle="collapse" href="#collapse_linked_doc">Linked Documents</a>
                 <?php
                 $harmonist_perm = ($current_user['harmonist_perms___10'] == 1) ? true : false;
                 $can_edit_linked_doc = UserEditConditions::canUserEditData($isAdmin, $current_user['record_id'], $concept['contact_link'], $concept['contact2_link'], $harmonist_perm);
@@ -623,7 +623,7 @@ if ((!empty($concept) && $concept['adminupdate_d'] != "" && count($concept['admi
             </h3>
         </div>
 
-        <div id="collapse_publications" class="table-responsive panel-collapse collapse in" aria-expanded="true">
+        <div id="collapse_linked_doc" class="table-responsive panel-collapse collapse in" aria-expanded="true">
             <table class="table table_requests sortable-theme-bootstrap" data-sortable id="abstracts">
                 <?php
                 if(!empty($concept['doc_title'])){
@@ -647,7 +647,7 @@ if ((!empty($concept) && $concept['adminupdate_d'] != "" && count($concept['admi
                     echo '</tr></thead>'.$header;
 
                     echo '<tbody>';
-                    foreach ($concept['dochidden_y'] as $linked_doc_instance => $doc_hidden_value){
+                    foreach ($concept['docupload_dt'] as $linked_doc_instance => $doc_hidden_value){
                         if($doc_hidden_value !== "1"){
                             echo '<tr>';
                             echo '<td width="15%">'.$concept['docupload_dt'][$linked_doc_instance].'</td>';
