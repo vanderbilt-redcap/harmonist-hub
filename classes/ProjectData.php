2ead471f3f90503ca2cea49d7e0300e267554b38
bascome <eva.bascompte.moragas@vumc.org>
2025-02-25 14:34:34 -0600

    fixed data check

--- classes/ProjectData.php
+++ classes/ProjectData.php
@@ -58,7 +58,7 @@ class ProjectData
                                         }
                                     }
                                     if (is_array($array[$index][$field_name]) &&
-                                        ProjectData::isCheckbox($field_name, $project_id) && is_array($value) && array_key_exists(1,$value)) {
+                                        ProjectData::isCheckbox($field_name, $project_id) && is_array($value) && array_key_exists(1,$value) && !empty($value[1])) {
                                         $array[$index][$field_name][$instance] = $value[1];
                                     }
                                 }
