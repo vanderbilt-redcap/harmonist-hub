99e8059440ba812b3d525244355b043027760763
bascome <eva.bascompte.moragas@vumc.org>
2025-04-04 15:01:40 -0500

    testing dnd

--- classes/SecurityHandler.php
+++ classes/SecurityHandler.php
@@ -255,30 +255,34 @@ class SecurityHandler
 
     function getTokenByUserId($userid): ?string
     {
-//        $people = REDCap::getData(
-//            $this->pidsArray['PEOPLE'],
-//            'json-array',
-//            null,
-//            array('access_token'),
-//            null,
-//            null,
-//            false,
-//            false,
-//            false,
-//            "[redcap_name] = '" . $userid . "' AND [active_y] = '1'"
-//        )[0];
+        $people = REDCap::getData(
+            $this->pidsArray['PEOPLE'],
+            'json-array',
+            null,
+            array('access_token'),
+            null,
+            null,
+            false,
+            false,
+            false,
+            "[redcap_name] = '" . $userid . "'"
+        )[0];
+        if($userid == "bascome"){
+            print_array("[redcap_name] = '" . $userid . "'");
+            print_array($people);
+        }
 
         $params = [
             'project_id' => $this->pidsArray['PEOPLE'],
             'return_format' => 'json-array',
             'fields' => ['access_token'],
-            'filterLogic' => "[redcap_name] = '" . $userid . "' AND [active_y] = '1'",
+            'filterLogic' => "[redcap_name] = '" . $userid . "' AND [active_y] = 1",
             'filterType' => "RECORD"
         ];
         $people = $this->module->escape(REDCap::getData($params))[0];
 
         if($userid == "bascome"){
-            print_array("[redcap_name] = '" . $userid . "' AND [active_y] = '1'");
+            print_array("[redcap_name] = '" . $userid . "' AND [active_y] = 1");
             print_array($people);
         }
         if (!empty($people)) {
