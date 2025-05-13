5b2cbc17fdedbcba9ac2fe10c395ceaad237751b
bascome <eva.bascompte.moragas@vumc.org>
2025-05-13 11:57:16 -0500

    testing session

--- sop_retrieve_data.php
+++ sop_retrieve_data.php
@@ -18,7 +18,6 @@ foreach ($request_DU as $down){
     }
     array_push($array_downloads_by_concept[$down['data_assoc_concept']][$down['data_assoc_request']],$down);
 }
-print_array($_SESSION);
 ?>
 <!DOCTYPE html>
 <html lang="en">
